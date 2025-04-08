efd676b2418d2ddbf7f5a464992606c6cd88757d
Zhou Yuhang <zyuhang255@gmail.com>
2025-04-08 18:54:59 +0800

    Create POST products endpoint to add new products

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,35 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
 
 dotenv.config();
 
 const app = express();
 
-app.get("/products", (req, res) => {});
+app.use(express.json()); // allows us to accept JSON data in the req.body
+
+app.post("/api/products", async (req, res) => {
+  const product = req.body; // user will send this data
+
+  if (!product.name || !product.price || !product.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please provide all fields" });
+  }
+
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.error("Error in Create product:", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
+// Postman desktop application
 
 app.listen(5000, () => {
   connectDB();
