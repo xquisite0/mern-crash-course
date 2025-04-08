ff0234457653855b0fda1c8652e0dd460658de34
Zhou Yuhang <zyuhang255@gmail.com>
2025-04-08 19:41:37 +0800

    Finish all CRUD API endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-import Product from "./models/product.model.js";
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
@@ -9,36 +9,7 @@ const app = express();
 
 app.use(express.json()); // allows us to accept JSON data in the req.body
 
-app.post("/api/products", async (req, res) => {
-  const product = req.body; // user will send this data
-
-  if (!product.name || !product.price || !product.image) {
-    return res
-      .status(400)
-      .json({ success: false, message: "Please provide all fields" });
-  }
-
-  const newProduct = new Product(product);
-
-  try {
-    await newProduct.save();
-    res.status(201).json({ success: true, data: newProduct });
-  } catch (error) {
-    console.error("Error in Create product:", error.message);
-    res.status(500).json({ success: false, message: "Server Error" });
-  }
-});
-
-app.delete("/api/products/:id", async (req, res) => {
-  const { id } = req.params;
-
-  try {
-    await Product.findByIdAndDelete(id);
-    res.status(200).json({ success: true, message: "Product deleted" });
-  } catch (error) {
-    res.status(404).json({ success: false, message: "Product not found" });
-  }
-});
+app.use("/api/products", productRoutes);
 
 app.listen(5000, () => {
   connectDB();
