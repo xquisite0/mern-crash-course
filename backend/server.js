8e45e36eb165bb0acbde784506310cc3d249b57a
Zhou Yuhang <zyuhang255@gmail.com>
2025-04-08 19:03:10 +0800

    Add DELETE Product endpoint

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,16 @@ app.post("/api/products", async (req, res) => {
   }
 });
 
-// Postman desktop application
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({ success: true, message: "Product deleted" });
+  } catch (error) {
+    res.status(404).json({ success: false, message: "Product not found" });
+  }
+});
 
 app.listen(5000, () => {
   connectDB();
